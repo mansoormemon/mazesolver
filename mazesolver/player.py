5326cd6f6a669b00d165200f0b4199906134755e
Mansoor Ahmed Memon <mansoorahmed.one@gmail.com>
2023-01-14 12:24:44 +0500

    Set direction of tank face

--- mazesolver/player.py
+++ mazesolver/player.py
@@ -1,34 +1,35 @@
 import arcade as arc
 
 
-PLAYER_SKIN = (
-    ":resources:images/topdown_tanks/tank_blue.png"
-)
+PLAYER_SKIN = ":resources:images/topdown_tanks/tank_blue.png"
 MOVEMENT_SPEED = 2
 
 
-class TextureTypeList:
-    LEFT_FACE = 0
-    RIGHT_FACE = 1
+FACE_UP = 180
+FACE_DOWN = 0
+FACE_LEFT = 270
+FACE_RIGHT = 90
 
 
 class Player(arc.Sprite):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.textures = arc.load_texture_pair(PLAYER_SKIN)
-        self.set_texture(TextureTypeList.LEFT_FACE)
+        self.texture = arc.load_texture(PLAYER_SKIN)
+        self.textures.append(self.texture)
 
     def move_up(self):
         self.change_y = MOVEMENT_SPEED
+        self.angle = FACE_UP
 
     def move_down(self):
         self.change_y = -MOVEMENT_SPEED
+        self.angle = FACE_DOWN
 
     def move_left(self):
         self.change_x = -MOVEMENT_SPEED
-        self.set_texture(TextureTypeList.LEFT_FACE)
+        self.angle = FACE_LEFT
 
     def move_right(self):
         self.change_x = MOVEMENT_SPEED
-        self.set_texture(TextureTypeList.RIGHT_FACE)
+        self.angle = FACE_RIGHT
